a4a2d4b17051abbe58694f5cc03adf427daae5d5
John Meadow <armitage3@gazeta.pl>
2021-01-25 17:45:27 +0100

    added hp_bar

--- source/zajecia_1/main.gd
+++ source/zajecia_1/main.gd
@@ -5,20 +5,28 @@ export(int) var max_hp = 500
 var text = "iterator = "
 
 onready var hit_points_label = $HP
+onready var hit_points_bar = $HP_bar
+onready var hit_points_bar2 = $HP_bar2
 
 func _ready():
-	hp += 1
+	hit_points_bar.max_value = max_hp
+	hit_points_bar2.max_value = max_hp
+#	hp += 1
 #	print (text, iterator)
 #	print ("iterator = " + str(iterator) )
-	print ("iterator = " + str(hp) )
+#	print ("iterator = " + str(hp) )
 
-	for i in 10:
-		print(i)
-		hit_points_label.text = "HP = " + str(i)
-
+#	for i in 10:
+#		print(i)
+#		hit_points_label.text = "HP = " + str(i)
+	pass
+	
 func _process(delta):
 	if hp < max_hp:
 		hp += 1
 		
-	if hp % 10 == 0:
-		hit_points_label.text = "HP = " + str(hp)
+	hit_points_bar.value = hp
+	hit_points_bar2.value = hp
+		
+#	if hp % 10 == 0:
+#		hit_points_label.text = "HP = " + str(hp)
