438c573fbb2c6f6eb13e33a0e8c2968157d37d66
John Meadow <armitage3@gazeta.pl>
2021-01-25 17:35:41 +0100

    Renamed variable

--- source/zajecia_1/main.gd
+++ source/zajecia_1/main.gd
@@ -1,22 +1,24 @@
 extends Node2D
 
-var iterator = 100
+var hp = 100
+export(int) var max_hp = 500
 var text = "iterator = "
 
-onready var hit_points = $HP
+onready var hit_points_label = $HP
 
 func _ready():
-	iterator += 1
+	hp += 1
 #	print (text, iterator)
 #	print ("iterator = " + str(iterator) )
-	print ("iterator = " + str(iterator) )
+	print ("iterator = " + str(hp) )
 
 	for i in 10:
 		print(i)
-		hit_points.text = "HP = " + str(i)
+		hit_points_label.text = "HP = " + str(i)
 
 func _process(delta):
-	
-	iterator += 1
-	if iterator % 13 == 0:
-		hit_points.text = "HP = " + str(iterator)
+	if hp < max_hp:
+		hp += 1
+		
+	if hp % 10 == 0:
+		hit_points_label.text = "HP = " + str(hp)
