b42c25b6a62ce6c9eb9e748cbc329bd387ff6a23
John Meadow <armitage3@gazeta.pl>
2021-04-11 10:25:07 +0200

    added friction

--- source/warszaty_programowanie/objects/player/Player.gd
+++ source/warszaty_programowanie/objects/player/Player.gd
@@ -5,8 +5,10 @@ onready var engine_animator  := $AnimationPlayer
 onready var engine_animator2 := $AnimationPlayer2
 onready var ship := $body
 
-const ENGINE_THRUST := 100.0
-const ANGULAR_ACCELERATION := 10.0
+const ENGINE_THRUST := 1000.0
+const ANGULAR_ACCELERATION := 20.0
+const VELOCITY_DAMPING := 1.0
+const ANGULAT_VELOCITY_DAMPING := 2.5
 const mass = 10
 
 # prędkosć
@@ -14,6 +16,7 @@ var velocity := Vector2.ZERO
 var speed    := 0.0 
 
 var is_thrusting := 0.0 
+#var is_braking   := 0.0 
 
 # obrót 
 var facing := Vector2.RIGHT
@@ -29,6 +32,7 @@ func _ready():
 func _process( delta ):
 	process_input( delta )
 	
+	anglular_velocity -= anglular_velocity * ANGULAT_VELOCITY_DAMPING * delta
 	orientation += anglular_velocity * delta
 	facing = Vector2( cos(orientation), sin(orientation) )
 	
@@ -38,9 +42,15 @@ func _process( delta ):
 		gravity_direction = gravity_object.global_position - global_position
 		distance = gravity_direction.length_squared() + pow(gravity_object.radius, 2)
 		gravity +=  (mass + gravity_object.mass) / distance * gravity_direction.normalized()
-		
 	
+	velocity -= velocity * VELOCITY_DAMPING * delta
+	
+#	if abs(is_thrusting) > 0:
 	velocity += (facing * is_thrusting * ENGINE_THRUST + gravity) * delta 
+#	elif speed > 0:
+#		velocity += (-facing * ENGINE_THRUST * 0.1 + gravity) * delta 
+		
+	speed = velocity.length()
 	position += velocity * delta
 	
 	ship.rotation = orientation
